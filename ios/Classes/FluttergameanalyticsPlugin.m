350c2b680ae46c30a3e1870b4da622c270cd3a9b
Joshua Matta <josh@janaspandana.in>
2020-05-10 05:53:28 +0530

    Bug fixes for iOS

--- ios/Classes/FluttergameanalyticsPlugin.m
+++ ios/Classes/FluttergameanalyticsPlugin.m
@@ -1,12 +1,5 @@
 #import "FluttergameanalyticsPlugin.h"
-#if __has_include(<fluttergameanalytics/fluttergameanalytics-Swift.h>)
-#import <fluttergameanalytics/fluttergameanalytics-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "fluttergameanalytics-Swift.h"
-#endif
+#import <flutter_game_analytics/flutter_game_analytics-Swift.h>
 
 @implementation FluttergameanalyticsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
